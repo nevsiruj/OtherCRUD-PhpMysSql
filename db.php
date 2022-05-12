66651bbfc25969ce3bd0bb664e9776e6f455e8e8
Gonzalo Suárez <98376961+nevsiruj@users.noreply.github.com>
2022-05-11 23:58:29 -0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'php_mysql_crud'
+);
+
+
+// if (isset($conn)) {
+//     echo 'DB is connected';
+// }
+
+?>
---
+++ delete_task.php
@@ -0,0 +1,20 @@
+<?php
+
+
+include('db.php');
+
+
+    if(isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "DELETE FROM task WHERE id = $id";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            die("Query Failed");
+        }
+
+        $_SESSION['message'] = 'Task deleted successfully';
+        $_SESSION['message_type'] = 'danger'; //alert
+        header("Location: index.php");
+    }
+
+?>
